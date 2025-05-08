27b7195141b81d513b053ce2ec4f86021609fca7
IAFEnvoy <tanksheng545@126.com>
2025-05-08 10:59:59 +0800

    remove unused import

--- src/main/java/com/iafenvoy/iafpatcher/mixin/TitleScreenMixin.java
+++ src/main/java/com/iafenvoy/iafpatcher/mixin/TitleScreenMixin.java
@@ -15,10 +15,8 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
