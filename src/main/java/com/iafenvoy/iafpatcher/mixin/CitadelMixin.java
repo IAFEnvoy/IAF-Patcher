c98e997bbcb1e0f11c9c7ca572abff5a9685fbaa
IAFEnvoy <tanksheng545@126.com>
2025-08-29 15:34:43 +0800

    update citadel to latest

--- src/main/java/com/iafenvoy/iafpatcher/mixin/CitadelMixin.java
+++ src/main/java/com/iafenvoy/iafpatcher/mixin/CitadelMixin.java
@@ -5,28 +5,46 @@ import com.github.alexthe666.citadel.web.WebHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
-@Mixin(value = Citadel.class, remap = false)
+@SuppressWarnings({"LoggingSimilarMessage", "CallToPrintStackTrace"})
+@Mixin(value = WebHelper.class, remap = false)
 public class CitadelMixin {
-    @Inject(method = "setup", at = @At(value = "INVOKE", target = "Lcom/github/alexthe666/citadel/web/WebHelper;getURLContents(Ljava/lang/String;Ljava/lang/String;)Ljava/io/BufferedReader;"), cancellable = true)
-    private void wrapPatreonListToSingleThread(CallbackInfo ci) {
-        ci.cancel();
+    @Inject(method = "getURLContents", at = @At("HEAD"), cancellable = true)
+    private static void wrapPatreonListToSingleThread(String urlString, String backupFileLoc, CallbackInfoReturnable<BufferedReader> cir) {
+        cir.setReturnValue(null);
         new Thread(() -> {
-            BufferedReader urlContents = WebHelper.getURLContents("https://raw.githubusercontent.com/Alex-the-666/Citadel/master/src/main/resources/assets/citadel/patreon.txt", "assets/citadel/patreon.txt");
+            BufferedReader urlContents = null;
+            try {
+                URL url = new URL(urlString);
+                URLConnection connection = url.openConnection();
+                connection.setConnectTimeout(3000);
+                connection.setReadTimeout(3000);
+                InputStream stream = connection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(stream);
+                urlContents = new BufferedReader(reader);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             if (urlContents != null) {
                 String line;
                 try {
                     while ((line = urlContents.readLine()) != null) {
                         Citadel.PATREONS.add(line);
                     }
-                } catch (IOException var5) {
+                } catch (IOException var3) {
                     Citadel.LOGGER.warn("Failed to load patreon contributor perks");
                 }
-            } else Citadel.LOGGER.warn("Failed to load patreon contributor perks");
+            } else {
+                Citadel.LOGGER.warn("Failed to load patreon contributor perks");
+            }
         }).start();
     }
 }
